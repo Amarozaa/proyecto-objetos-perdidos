a281ef2eedb31af92366d28396561d2d3abfd1e3
Amaro Zurita <amarozurita19@gmail.com>
2025-09-16 14:25:31 -0300

    WIP de formulario

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./pages/Inicio";
 import ListadoObjetosPerdidos from "./pages/ListadoObjetosPerdidos";
 import FormularioPublicacion from "./pages/FormularioPublicacion";
+import { Usuario, Publicacion } from "./types/types";
+
+
+const usuarioDemo: Usuario = {
+  id: 1,
+  nombre: "Amaro Zurita",
+  email: "amaro@uc.cl",
+  password: "1234",
+};
 
 function App() {
   return (
@@ -9,7 +18,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/listado" element={<ListadoObjetosPerdidos />} />
-        <Route path="/formulario" element={<FormularioPublicacion />} />
+  <Route path="/formulario" element={<FormularioPublicacion usuario={usuarioDemo} onPublicacionCreada={() => {}} />} />
       </Routes>
     </BrowserRouter>
   );
